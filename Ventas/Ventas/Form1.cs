21fcb1f6350a23303fb5fb5515c178fdc438a5d9
oscar <oscar@DESKTOP-A6AJCNO>
2019-12-12 22:51:45 -0600

    Agregando frase motivadora.

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo!");
+            MessageBox.Show("Piensa, cree, sueña y atrevete");
         }
     }
 }
