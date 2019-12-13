76120e9b49e30e1ee3936346c2dfbda85f46b3f7
oscar <oscar@DESKTOP-A6AJCNO>
2019-12-13 00:00:58 -0600

    Agregando clase producto y clase categoria.

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,44 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Piensa, cree, sueña y atrevete");
+            //Categorias
+            var categoria1 = new Categoria();
+            categoria1.Id = 1;
+            categoria1.Nombre = "Celulares";
+
+            var categoria2 = new Categoria();
+            categoria2.Id = 1;
+            categoria2.Nombre = "Accesorios";
+
+            //Productos
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descipcion = "Celular iPhone";
+            producto1.Precio = 10000;
+            producto1.Categoria = categoria1;
+          
+            Producto producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descipcion = "Celular Samsung";
+            producto2.Precio = 8000;
+            producto2.Categoria = categoria1;
+
+            Producto producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descipcion = "Audifonos";
+            producto3.Precio = 300;
+            producto3.Categoria = categoria2;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            foreach (var p in listadeProductos)
+            {
+                MessageBox.Show(p.Descipcion+ " " + p.Categoria.Nombre);
+            }
+            
         }
     }
 }
---
+++ Ventas/Ventas/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Descipcion { get; set; }
+        public double Precio { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
